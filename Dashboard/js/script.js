65ce94364ee99f2a13297bf18a51a0123d95830d
Otavio Esteves <otaviob925@gmail.com>
2024-04-04 17:03:27 -0300

    Adding Hidden/Show Function

--- Dashboard/js/script.js
+++ Dashboard/js/script.js
@@ -2,3 +2,30 @@ const sidebarToggle = document.querySelector("#sidebar-toggle");
 sidebarToggle.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
+
+const studentsToggle = document.querySelector("#alunos-item");
+studentsToggle.addEventListener("click",function(){
+    document.querySelector("#alunos-item").classList.add("active");
+    document.querySelector("#turmas-item").classList.remove("active");
+    document.querySelector("#coordenadores-item").classList.remove("active");
+    document.querySelector("#list-screen").classList.add("show");
+    document.querySelector("#turmas-screen").classList.remove("show");
+});
+
+const coordenadoresToggle = document.querySelector("#coordenadores-item");
+coordenadoresToggle.addEventListener("click",function(){
+    document.querySelector("#coordenadores-item").classList.add("active");
+    document.querySelector("#turmas-item").classList.remove("active");
+    document.querySelector("#alunos-item").classList.remove("active");
+    document.querySelector("#list-screen").classList.add("show");
+    document.querySelector("#turmas-screen").classList.remove("show");
+});
+
+const classesToggle = document.querySelector("#turmas-item");
+classesToggle.addEventListener("click",function(){
+    document.querySelector("#turmas-item").classList.add("active");
+    document.querySelector("#alunos-item").classList.remove("active");
+    document.querySelector("#coordenadores-item").classList.remove("active");
+    document.querySelector("#turmas-screen").classList.add("show");
+    document.querySelector("#list-screen").classList.remove("show");
+});
