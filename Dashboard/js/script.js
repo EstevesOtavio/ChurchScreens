0d61cd831b5148629e1d072b49827cb742b68882
otavioe <otaviob925@gmail.com>
2024-04-15 01:47:12 -0300

    Updating Screens

--- Dashboard/js/script.js
+++ Dashboard/js/script.js
@@ -3,8 +3,8 @@ sidebarToggle.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
 
-const studentsToggle = document.querySelector("#alunos-item");
-studentsToggle.addEventListener("click",function(){
+const studentsItem = document.querySelector("#alunos-item");
+studentsItem.addEventListener("click",function(){
     document.querySelector("#alunos-item").classList.add("active");
     document.querySelector("#turmas-item").classList.remove("active");
     document.querySelector("#coordenadores-item").classList.remove("active");
@@ -16,39 +16,54 @@ studentsToggle.addEventListener("click",function(){
     document.querySelector("#list-coordenadores-title").classList.remove("show")
     document.querySelector("#btn-adicionar-aluno").classList.add("show")
     document.querySelector("#btn-adicionar-coordenador").classList.remove("show")
+    document.querySelector("#main-addTurma").classList.remove("show");
 });
 
-const coordenadoresToggle = document.querySelector("#coordenadores-item");
-coordenadoresToggle.addEventListener("click",function(){
+const coordenadoresItem = document.querySelector("#coordenadores-item");
+coordenadoresItem.addEventListener("click",function(){
     document.querySelector("#coordenadores-item").classList.add("active");
     document.querySelector("#turmas-item").classList.remove("active");
     document.querySelector("#alunos-item").classList.remove("active");
     document.querySelector("#list-screen").classList.add("show");
     document.querySelector("#turmas-screen").classList.remove("show");
-    document.querySelector("#pesquisar-aluno").classList.remove("show")
-    document.querySelector("#pesquisar-coordenador").classList.add("show")
-    document.querySelector("#list-alunos-title").classList.remove("show")
-    document.querySelector("#list-coordenadores-title").classList.add("show")
-    document.querySelector("#btn-adicionar-aluno").classList.remove("show")
-    document.querySelector("#btn-adicionar-coordenador").classList.add("show")
+    document.querySelector("#pesquisar-aluno").classList.remove("show");
+    document.querySelector("#pesquisar-coordenador").classList.add("show");
+    document.querySelector("#list-alunos-title").classList.remove("show");
+    document.querySelector("#list-coordenadores-title").classList.add("show");
+    document.querySelector("#btn-adicionar-aluno").classList.remove("show");
+    document.querySelector("#btn-adicionar-coordenador").classList.add("show");
+    document.querySelector("#main-addTurma").classList.remove("show");
 });
 
-const classesToggle = document.querySelector("#turmas-item");
-classesToggle.addEventListener("click",function(){
+const classesItem = document.querySelector("#turmas-item");
+classesItem.addEventListener("click",function(){
     document.querySelector("#turmas-item").classList.add("active");
     document.querySelector("#alunos-item").classList.remove("active");
     document.querySelector("#coordenadores-item").classList.remove("active");
     document.querySelector("#turmas-screen").classList.add("show");
     document.querySelector("#list-screen").classList.remove("show");
     document.querySelector("#main-perfilStudent").classList.remove("show");
+    document.querySelector("#main-addTurma").classList.remove("show");
 });
 
-const perfilStudentToggle = document.querySelector("#btn-adicionar-aluno");
-perfilStudentToggle.addEventListener("click",function(){
+const perfilStudentBtn = document.querySelector("#btn-adicionar-aluno");
+perfilStudentBtn.addEventListener("click",function(){
     document.querySelector("#turmas-item").classList.remove("active");
     document.querySelector("#alunos-item").classList.add("active");
     document.querySelector("#coordenadores-item").classList.remove("active");
     document.querySelector("#turmas-screen").classList.remove("show");
     document.querySelector("#list-screen").classList.remove("show");
     document.querySelector("#main-perfilStudent").classList.add("show");
+    document.querySelector("#main-addTurma").classList.remove("show");
+});
+
+const addTurmaBtn = document.querySelector("#btn-addTurma");
+addTurmaBtn.addEventListener("click",function(){
+    document.querySelector("#turmas-item").classList.add("active");
+    document.querySelector("#alunos-item").classList.remove("active");
+    document.querySelector("#coordenadores-item").classList.remove("active");
+    document.querySelector("#turmas-screen").classList.remove("show");
+    document.querySelector("#list-screen").classList.remove("show");
+    document.querySelector("#main-perfilStudent").classList.remove("show");
+    document.querySelector("#main-addTurma").classList.add("show");
 });
