fd10086912aa8833f66efe8f8bba565e0106ea96
otavio_esteves <otaviob925@gmail.com>
2024-09-22 19:53:29 -0300

    Correcting Title Catequistas to Catequisandos

---
+++ Dashboard/js-standart/standart.js
@@ -0,0 +1,24 @@
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+sidebarToggle.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+function executarTarefaPorTamanhoTela() {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+  
+    function verificarTamanhoTela(e) {
+      if (e.matches) {
+        document.querySelector("#sidebar").classList.toggle("collapsed");
+        console.log("A largura da tela é menor ou igual a 768px. Executando tarefa...");
+        // Aqui você pode colocar a tarefa que deseja executar
+      } else {
+        console.log("A largura da tela é maior que 768px.");
+      }
+    }
+  
+    // Verifica o tamanho da tela inicialmente
+    verificarTamanhoTela(mediaQuery);
+}
+  
+executarTarefaPorTamanhoTela();
+  
